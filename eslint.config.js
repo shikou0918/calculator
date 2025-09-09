fdf2d2e77955edc28d20172a44d6b59de90f2856
shikou0918 <shikou.0918@gmail.com>
2025-09-10 07:35:43 +0900

    Prettier及びlinter導入

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import js from '@eslint/js'
+import typescript from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import vue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+  js.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.{js,ts,vue}'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: typescriptParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue']
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      vue,
+      prettier
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'vue/multi-word-component-names': 'off',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'vue/html-self-closing': ['error', {
+        html: { void: 'always', normal: 'always', component: 'always' },
+        svg: 'always',
+        math: 'always'
+      }]
+    }
+  },
+  {
+    files: ['**/*.test.{js,ts}', '**/*.spec.{js,ts}'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
+]
